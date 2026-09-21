6209a7744b033b7ee52a32d38e91ffc03c148df5
kosanasreeharsha44-ctrl <kosanasreeharsha44@gmail.com>
2026-09-21 22:24:20 +0530

    Update Q2_StudentHashing.c

--- assignment/Q2_StudentHashing.c
+++ assignment/Q2_StudentHashing.c
@@ -13,29 +13,34 @@ int main(){
 
     for(i=0;i<n;i++){
         h=keys[i]%SIZE;
-        if(count[h]>0) printf("Collision: Roll No. %d at index %d\n",keys[i],h);
+        if(count[h]>0) 
+            printf("Collision: Roll No. %d at index %d\n",keys[i],h);
         chain[h][count[h]++]=keys[i];
     }
 
     printf("\nOPEN HASHING - Student Records\n");
     for(i=0;i<SIZE;i++){
         printf("Index %d: ",i);
-        for(j=0;j<count[i];j++) printf("%d ",chain[i][j]);
+        for(j=0;j<count[i];j++) 
+            printf("%d ",chain[i][j]);
         printf("\n");
     }
 
     for(i=0;i<n;i++){
         h=keys[i]%SIZE;
         j=h;
-        while(used[j]) j=(j+1)%SIZE;
+        while(used[j]) 
+            j=(j+1)%SIZE;
         table[j]=keys[i];
         used[j]=1;
     }
 
     printf("\nCLOSED HASHING - Linear Probing\n");
     for(i=0;i<SIZE;i++)
-        if(table[i]!=-1) printf("Index %d: Roll No. %d\n",i,table[i]);
-        else printf("Index %d: Empty\n",i);
+        if(table[i]!=-1) 
+            printf("Index %d: Roll No. %d\n",i,table[i]);
+        else 
+            printf("Index %d: Empty\n",i);
 
     printf("\nOpen hashing is more flexible when student records increase.\n");
     return 0;
