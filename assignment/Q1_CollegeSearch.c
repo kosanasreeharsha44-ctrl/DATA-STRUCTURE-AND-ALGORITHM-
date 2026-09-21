563a600a22ca14a7b8c48cd85e882f1aff725dff
kosanasreeharsha44-ctrl <kosanasreeharsha44@gmail.com>
2026-09-21 22:23:00 +0530

    Update Q1_CollegeSearch.c

--- assignment/Q1_CollegeSearch.c
+++ assignment/Q1_CollegeSearch.c
@@ -11,22 +11,31 @@ int main(){
     scanf("%d",&key);
 
     for(i=0;i<n;i++){
-        if(roll[i]==key){seq=i;break;}
+        if(roll[i]==key){
+            seq=i;break;
+        }
     }
 
     low=0;high=n-1;
     while(low<=high){
         mid=(low+high)/2;
-        if(roll[mid]==key){bin=mid;break;}
-        if(roll[mid]<key) low=mid+1;
+        if(roll[mid]==key){
+            bin=mid;break;
+        }
+        if(roll[mid]<key) 
+            low=mid+1;
         else high=mid-1;
     }
 
     printf("\nRegistration Search Result\n");
-    if(seq!=-1) printf("Student Roll No. %d found by Sequential Search.\n",key);
-    else printf("Student Roll No. %d not found by Sequential Search.\n",key);
-    if(bin!=-1) printf("Student Roll No. %d found by Binary Search.\n",key);
-    else printf("Student Roll No. %d not found by Binary Search.\n",key);
+    if(seq!=-1) 
+        printf("Student Roll No. %d found by Sequential Search.\n",key);
+    else 
+        printf("Student Roll No. %d not found by Sequential Search.\n",key);
+    if(bin!=-1) 
+        printf("Student Roll No. %d found by Binary Search.\n",key);
+    else 
+        printf("Student Roll No. %d not found by Binary Search.\n",key);
     printf("Binary Search is suitable for repeatedly searching sorted records.\n");
     return 0;
 }
